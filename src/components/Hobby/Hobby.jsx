d50b88299b2229587ea8f3effcf0d8ba7c56dff5
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-10-15 12:31:11 -0400

    add hobby section with selectable hobby

--- src/components/Hobby/Hobby.jsx
+++ src/components/Hobby/Hobby.jsx
@@ -1,35 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import PortfolioContext from '../../context/context';
+import { useEffect } from 'react';
 
 const Hobby = () => {
   const { hobby } = useContext(PortfolioContext);
   const { title, hobbies } = hobby;
 
+  const [selectedHobby, setSelectedHobby] = useState({});
+
+  useEffect(() => {
+    if (!selectedHobby.key) {
+      const defaultHobby = hobbies && hobbies[0] ? hobbies[0] : {};
+      setSelectedHobby(defaultHobby);
+    }
+  });
+
   return (
     <section id="hobby">
       <Container>
-        <h1 className="hobby-title">
+        <h1 className="section-title hobby-title">
           { title }
         </h1>
-        <div className="hobby-list">
-          {hobbies.map((hobby) => {
-            const { key, name, description, images, action, link } = hobby;
+        <Row noGutters>
+          <Col xs={11} lg={4}>
+            <Row>
+              {hobbies.map((hobby) => {
+                const { key, name } = hobby;
+                const classNames = (key === selectedHobby.key) ? 'hobby-name hobby-name--selected' : 'hobby-name';
 
-            return (
-              <Row key={key}>
-                <Col>
-                  <h4>{ name }</h4>
-                  <div>
-                      {description}
-                  </div>
-                  <a href={link} target="_blank">{ action }</a>
-                </Col>
-              </Row>
-            );
-          })}
-        </div>
+                return (
+                  <Col xs lg={12} key={key}>
+                    <div className={classNames} onClick={() => { setSelectedHobby(hobby) }}>
+                      { name }
+                    </div>
+                  </Col>
+                );
+              })}
+            </Row>
+          </Col>
+          <Col xs={12} lg={6}>
+            <div className="hobby-content">
+              <div className="hobby-image">
+              </div>
+              <div className="hobby-description">
+                { selectedHobby.description }
+              </div>
+              <a href={ selectedHobby.link } target="_blank" className="hobby-link">{ selectedHobby.action }</a>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </section>
   );
